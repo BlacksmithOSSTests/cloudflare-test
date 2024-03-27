fb61608de44eecfa9fe48e89edd38e3029247d02
Aditya Maru <adityamaru@gmail.com>
2024-03-26 20:53:05 -0400

    wip

---
+++ start-worker.js
@@ -0,0 +1,14 @@
+const { unstable_dev } = require("wrangler");
+
+async function startWorker() {
+    const worker = await unstable_dev('src/index.ts', {
+        experimental: { disableExperimentalWarning: true },
+        persist: true,
+        port: 8767,
+        compatibilityFlags: ['nodejs_compat', 'streams_enable_constructors'],
+    });
+    console.log("Worker started");
+    // Optionally, keep the process running or perform additional checks here
+}
+
+startWorker().catch(console.error);
